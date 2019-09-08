adb1106316ad6df46b8fcecc033dc64bfc12935f
Derek Meer <derekmeer@protonmail.com>
2019-09-07 22:15:43 -0700

    footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let small = false
+</script>
+
+<style>
+  div > * {
+    @apply mb-0 mx-2;
+  }
+  div.flex-col > * {
+    @apply mb-2 mx-0;
+  }
+</style>
+
+<hr />
+<div
+  class="flex text-sm justify-center items-center text-gray-500 py-4"
+  class:flex-col={small}>
+  <p class="text-center">Copyright 2019 BadukClub.</p>
+  <p class="text-center" class:-mt-2={small}>All rights reserved.</p>
+  <a href="docs/terms-of-service" class="underline hover:text-gray-800">
+    Terms
+  </a>
+  <a href="docs/privacy-policy" class="underline hover:text-gray-800">
+    Privacy Policy
+  </a>
+  <a href="mailto:support@baduk.club" class="underline hover:text-gray-800">
+    Support
+  </a>
+</div>
