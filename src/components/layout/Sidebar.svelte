1ba9bf998d598396079a06731a52138c1c89af4f
Derek Meer <derekmeer@protonmail.com>
2019-09-07 22:19:50 -0700

    add Sidebar component

---
+++ src/components/layout/Sidebar.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Footer from './Footer.svelte'
+</script>
+
+<style>
+  div.sidebar {
+    @apply fixed w-56 bg-white left-0 pt-12 z-50 flex flex-col justify-between;
+    top: 4rem;
+    height: calc(100vh - 4rem);
+  }
+</style>
+
+<div class="sidebar">
+  <div>
+    <slot />
+  </div>
+  <Footer small />
+</div>
