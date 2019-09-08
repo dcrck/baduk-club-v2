fd0900f19be43d727bc9a952d75340d230d4e862
Derek Meer <derekmeer@protonmail.com>
2019-09-07 18:41:02 -0700

    icons

---
+++ src/components/Icon.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let size = 24
+  export let color = '#262621'
+  export let id
+  export let fill = 'none'
+</script>
+
+<style>
+  .feather {
+    stroke-width: 2;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+  }
+</style>
+
+<svg class="feather {id}" width={size} height={size} stroke={color} {fill}>
+  <use xlink:href="feather-sprite.svg#{id}" />
+</svg>
