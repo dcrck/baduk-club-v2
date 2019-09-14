6d9b191253fada7834a577d7f850bf4af4e1c9e6
Derek Meer <derekmeer@protonmail.com>
2019-09-13 19:52:25 -0700

    Event Form v1
    This commit adds the first version of the event form, allowing a user to
    add meeting times, a name, a location, description, and "private"
    status.

---
+++ cypress/integration/forms/event.spec.js
@@ -0,0 +1,76 @@
+describe('Event form', () => {
+  before(() => {
+    cy.visit('test/EventForm')
+  })
+
+  it('only allows a single meeting time entry for one-time meetups', () => {
+    cy.get('label[for="__once__"]')
+      .wait(500)
+      .click()
+      .click()
+    cy.get('[data-cy="start-date"]').type('2019-09-01')
+    cy.get('[data-cy="end-date"]').type('2019-09-01')
+    cy.get('[data-cy="start-time"]').type('12:00')
+    cy.get('[data-cy="end-time"]').type('13:00')
+    cy.get('[data-cy="add-time"]').click()
+    cy.get('[data-cy="add-time"]').should('not.exist')
+    cy.get('[data-cy="cancel-event-form"]').click()
+  })
+
+  it('prevents no name', () => {
+    cy.get('#name').clear()
+    cy.get('[data-cy="submit-event-form"]').should('be.disabled')
+    cy.get('[data-cy="cancel-event-form"]').click()
+  })
+
+  it('prevents no address', () => {
+    cy.get('#address').clear()
+    cy.get('[data-cy="submit-event-form"]').should('be.disabled')
+    cy.get('[data-cy="cancel-event-form"]').click()
+  })
+
+  it('prevents invalid address', () => {
+    cy.get('#address')
+      .type('15198')
+      .blur()
+    cy.get('[data-cy="submit-event-form"]').should('be.disabled')
+    cy.get('[data-cy="cancel-event-form"]').click()
+  })
+
+  it('prevents clearing a valid address', () => {
+    cy.get('#address')
+      .clear()
+      .type('15198')
+    cy.get('[data-cy="search-result"]')
+      .first()
+      .click()
+    cy.get('#address')
+      .type('{backspace}')
+      .wait(500)
+      .blur()
+    cy.get('[data-cy="submit-event-form"]').should('be.disabled')
+    cy.get('[data-cy="cancel-event-form"]').click()
+  })
+
+  it('allows a user to create a meetup', () => {
+    cy.get('#address')
+      .clear()
+      .type('15198 Hook Hollow Road, Novelty')
+    cy.get('[data-cy="search-result"]')
+      .contains('15198 Hook Hollow')
+      .click()
+    cy.get('label[for="__weekly__"]').click()
+    cy.get('#name').type('Test Meetup')
+    cy.get('[data-cy="start-time"]').type('17:00')
+    cy.get('[data-cy="end-time"]').type('19:00')
+    cy.get('[data-cy="add-time"]')
+      .should('not.be.disabled')
+      .click()
+    cy.get('#description').type('Sample description!')
+    cy.get('#private')
+      .click()
+      .click()
+    cy.get('[data-cy="submit-event-form"]').should('not.be.disabled')
+    cy.get('[data-cy="cancel-event-form"]').click()
+  })
+})
---
+++ src/components/forms/Event.svelte
@@ -0,0 +1,155 @@
+<script>
+  import { createEventDispatcher } from 'svelte'
+  import LocationSearch from '/components/input/LocationSearch'
+  import TimeForm from '/components/forms/RecurrenceRule'
+  import Validation from '/components/input/Validation'
+  import Icon from '/components/Icon'
+  import check, { initialize } from '/components/input/validate'
+  import { toString } from '/utils/rrule'
+
+  const dispatch = createEventDispatcher()
+
+  let error = n => (!n ? 'Please enter a name for your meetup' : '')
+
+  export let initial = {
+    address: '',
+    times: [],
+    name: '',
+    is_private: false,
+    description: '',
+    geolocation: null,
+  }
+
+  let current, refresh, state
+  $: disabled = !(
+    JSON.stringify(initial) !== JSON.stringify(current) && state.status === 'ok'
+  )
+  reset()
+
+  function reset() {
+    current = JSON.parse(JSON.stringify(initial))
+    refresh ^= true
+    state = initialize(current.name, error)
+  }
+
+  function validate(n, first = false) {
+    state = check(first || state.status !== 'initial', error, n) || state
+  }
+
+  function submit() {
+    dispatch('submit', { data: current })
+    reset()
+  }
+
+  function cancel() {
+    dispatch('cancel')
+    reset()
+  }
+  $: current && validate(current.name)
+
+  const removeTime = i =>
+    (current.times = current.times
+      .slice(0, i)
+      .concat(current.times.slice(i + 1)))
+  const addTime = ({ detail: { time } }) =>
+    (current.times = [...current.times, time])
+
+  const selectLocation = ({ detail }) => (current = { ...current, ...detail })
+  const clearLocation = () =>
+    (current = { ...current, address: '', geolocation: null })
+</script>
+
+<style>
+  .form-label {
+    @apply pb-8;
+  }
+
+  .form-input {
+    @apply px-3 py-2 border rounded border-gray-400;
+  }
+
+  .form-input:focus {
+    @apply border-gray-800;
+  }
+</style>
+
+<div class="flex flex-col">
+  <label for="name" class="form-label">
+    Name
+    <Validation {...state}>
+      <input
+        type="text"
+        id="name"
+        data-cy="name-input"
+        class="form-input w-full"
+        bind:value={current.name}
+        on:blur={() => validate(current.name, true)} />
+    </Validation>
+  </label>
+
+  <label for="address" class="form-label">
+    Address
+    <LocationSearch
+      id="address"
+      initial={current.address}
+      on:select={selectLocation}
+      on:clear={clearLocation}
+      {refresh}
+      placeholder="Where are you meeting? (start typing!)" />
+  </label>
+
+  <label for="times">Meeting Time(s)</label>
+  {#each current.times as time, i}
+    <div class="flex items-center text-xl">
+      <span class="mr-4">{toString(time, !time.rrule)}</span>
+      <button
+        data-cy="remove-time"
+        class="opacity-50 hover:opacity-100"
+        on:click={() => removeTime(i)}>
+        <Icon id="x" />
+      </button>
+    </div>
+  {/each}
+
+  {#if current.times.length !== 1 || !current.times.length || current.times[0].rrule}
+    <div class="flex-0">
+      <TimeForm on:submit={addTime} />
+    </div>
+  {/if}
+
+  <label class="my-4 inline-block w-full" for="description">
+    Description
+    <textarea
+      bind:value={current.description}
+      id="description"
+      class="border-gray-400 w-full border focus:border-gray-800 rounded py-2
+      px-3"
+      placeholder="What do members need to know about your meetup?" />
+  </label>
+
+  <label class="my-4 inline-block w-full flex items-center" for="private">
+    <input
+      type="checkbox"
+      id="private"
+      class="-mt-px mr-2"
+      bind:value={current.is_private} />
+    This meetup is invite-only
+  </label>
+
+  <div class="flex items-center justify-between">
+    <button
+      data-cy="cancel-event-form"
+      class="bg-white border-2 border-gray-800 rounded px-4 py-2 mr-2"
+      on:click={cancel}>
+      Cancel
+    </button>
+    <button
+      {disabled}
+      data-cy="submit-event-form"
+      on:click={submit}
+      class="my-4 {disabled ? 'opacity-25 cursor-not-allowed ' : ''}px-4 py-2
+      bg-gray-800 rounded text-white">
+      Submit
+    </button>
+  </div>
+</div>
