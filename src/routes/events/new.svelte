e746517dd06ebf2db16e1733694948b04ebc7515
Derek Meer <derekmeer@protonmail.com>
2019-09-15 18:33:22 -0700

    add 'events/new' route for New Event Form
    This commit makes the New Event form accessible at its own route,
    'events/new'.

---
+++ src/routes/events/new.svelte
@@ -0,0 +1,12 @@
+<script context="module">
+  export async function preload({ path }, { user }) {
+    return user ? { user } : this.redirect(`login?redir=${path}`)
+  }
+</script>
+
+<script>
+  import NewEventForm from '/components/forms/NewEvent'
+  import { goto } from '@sapper/app'
+</script>
+
+<NewEventForm on:cancel={() => goto('/')} />
