78011482e59cf97f9f8a332d3a52a75995330305
Derek Meer <derekmeer@protonmail.com>
2019-09-15 22:35:40 -0700

    add database helper functions file
    The database helper functions file will provide queries for
    commonly-used database operations. As an example, there's a query called
    profileData which collects profile data for a user. This could be used
    in both the user profile page and a "other user" page.

---
+++ src/api/db/helpers.js
@@ -0,0 +1,28 @@
+import { merge } from '/api/db/index'
+
+export const profileData = id =>
+  merge([
+    {
+      root: 'games',
+      type: 'select',
+      filters: { where: { _or: { black: { _eq: id }, white: { _eq: id } } } },
+      fields: {
+        _: ['komi', 'winner', 'handicap'],
+        black_player: ['name', 'rank', 'id'],
+        white_player: ['name', 'rank', 'id'],
+        event: ['name', 'id'],
+      },
+    },
+    {
+      root: 'users',
+      type: 'select',
+      filters: { where: { id: { _eq: id } }, limit: 1 },
+      fields: {
+        _: ['id', 'rank', 'name', 'phone', 'picture'],
+        events: ['id', 'name', 'times', 'address'],
+        attendances: {
+          event: ['id', 'name', 'times', 'address', 'organizer_id'],
+        },
+      },
+    },
+  ])
