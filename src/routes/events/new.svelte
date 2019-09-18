960e02638ca18f6e257b050c2d0f449dd4aa6754
Derek Meer <derekmeer@protonmail.com>
2019-09-18 16:30:53 -0700

    prettier cleanup

--- src/routes/events/new.svelte
+++ src/routes/events/new.svelte
@@ -1,6 +1,6 @@
 <script context="module">
   export async function preload({ path }, { user }) {
-    if (!user)  return this.redirect(`login?redir=${path}`)
+    if (!user) return this.redirect(`login?redir=${path}`)
     if (!user.email_verified) return this.redirect('/')
     return { user }
   }
