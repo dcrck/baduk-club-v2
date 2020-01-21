f462e0b99192a54e336c628cfea69e753b60db86
Derek Meer <derekmeer@protonmail.com>
2020-01-21 14:39:04 -0800

    add location data API file
    This commit adds the location data API file, /api/location/index. It
    also moves the country data from the Map routes directory to this new
    API directory, which makes more sense. The Location Data API contains
    two methods:
    
    - getBoundingBox, which gets the bounding box given a country code. The
    /map/[country] route calls this function to retrieve the bounding box.
    - getCountry, which uses ipinfo.io to find the user's country for
    automatic centering of the map. This will be used in future versions of
    the map to automatically center on the user's country when they navigate
    to the map without a country in mind.

---
+++ src/api/location/index.js
@@ -0,0 +1,25 @@
+import countries from './data.json'
+
+export function getBoundingBox(code) {
+  if (!code) return undefined
+  if (code.length == 2) code = countries[code.toUpperCase()]
+  const data = countries[code.toUpperCase()]
+  if (!data) return undefined
+  const {
+    sw: { lat: s, lon: w },
+    ne: { lat: n, lon: e },
+    name,
+  } = data
+  return { name, code, bounds: [w, s, e, n] }
+}
+
+export const getCountry = (f = fetch) =>
+  f('https://ipinfo.io/json', {
+    method: 'GET',
+    headers: {
+      authorization: `Bearer ${process.env.IPINFO_TOKEN}`,
+      accept: 'application/json',
+    },
+  })
+    .then(r => r.json())
+    .then(({ country }) => country)
--- src/routes/map/[country].js
+++ src/routes/map/[country].js
@@ -1,20 +1,18 @@
-import countries from './_data.json'
+import { getBoundingBox } from '/api/location/index'
 
 export async function get(req, res) {
   let { country } = req.params
-  try {
-    if (country.length == 2) country = countries[country.toUpperCase()]
-    const {
-      sw: { lat: s, lon: w },
-      ne: { lat: n, lon: e },
-      name,
-    } = countries[country.toUpperCase()]
-    res.redirect(
-      `/map?code=${country.toLowerCase()}&name=${encodeURIComponent(
-        name
-      )}&bounds=${w},${s},${e},${n}`
-    )
-  } catch (e) {
-    res.redirect('/map')
-  }
+  const response = getBoundingBox(country)
+  if (!response) return res.redirect('/map')
+
+  const {
+    name,
+    code,
+    bounds: [w, s, e, n],
+  } = response
+  return res.redirect(
+    `/map?code=${code.toLowerCase()}&name=${encodeURIComponent(
+      name
+    )}&bounds=${w},${s},${e},${n}`
+  )
 }
